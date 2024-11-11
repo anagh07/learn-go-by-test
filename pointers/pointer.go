046d19b6d5901aa2931ea468e1391581be0a436b
Anagh Mehran <anagh@anagh-think.mint>
2024-11-10 16:38:14 -0800

    Stringer method for Bitcoin type

--- pointers/pointer.go
+++ pointers/pointer.go
@@ -2,15 +2,21 @@ package pointers
 
 import "fmt"
 
+type Bitcoin float64
+
 type Wallet struct {
-	balance float64
+	balance Bitcoin
 }
 
-func (wallet *Wallet) Deposit(amount float64) {
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%.2f BTC", b)
+}
+
+func (wallet *Wallet) Deposit(amount Bitcoin) {
 	fmt.Printf("address of balance in Deposit is %p\n", &wallet.balance)
 	wallet.balance += amount
 }
 
-func (wallet *Wallet) Balance() float64 {
+func (wallet *Wallet) Balance() Bitcoin {
 	return wallet.balance
 }
--- pointers/pointer_test.go
+++ pointers/pointer_test.go
@@ -7,22 +7,22 @@ import (
 
 func TestWallet(t *testing.T) {
 	wallet := Wallet{}
-	wallet.Deposit(10)
+	wallet.Deposit(Bitcoin(10))
 
 	got := wallet.Balance()
-	want := 10.0
+	want := Bitcoin(10)
 
 	fmt.Printf("address of balance in test is %p\n", &wallet.balance)
 
 	if got != want {
-		t.Errorf("got %f, want %f", got, want)
+		t.Errorf("got %s, want %s", got, want)
 	}
 
-	wallet.Deposit(15)
+	wallet.Deposit(Bitcoin(15.0))
 	got = wallet.Balance()
 	want += 15
 
 	if got != want {
-		t.Errorf("got %f, want %f", got, want)
+		t.Errorf("got %s, want %s", got, want)
 	}
 }
