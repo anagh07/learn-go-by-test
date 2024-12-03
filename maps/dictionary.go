3534b9a7333f7378f5de28a8596a7ad4d2d89602
Anagh Mehran <anagh07@gmail.com>
2024-12-03 01:15:53 -0800

    add existing key to map

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,5 +1,51 @@
 package maps
 
-func Search(dictionary map[string]string, word string) string {
-	return dictionary[word]
+import (
+	"fmt"
+)
+
+// in dictionaries, keys must be of comparable types
+// bool, int, float, complex, string, pointer, channel -> comparable
+// struct -> comparable (all fields are comparable)
+// array -> comparable (array elements are comparable)
+
+// maps can have nil value, and they read like empty maps
+// write attempt to nil value maps will give error
+
+type Dictionary map[string]string
+type DictionaryErr string
+
+const (
+	ErrNotFound  = DictionaryErr("key not found")
+	ErrKeyExists = DictionaryErr("key already exists")
+)
+
+func (dictionary Dictionary) Search(word string) (string, error) {
+	// map in go returns a second value of type boolean
+	value, found := dictionary[word]
+	fmt.Print(value)
+
+	if !found {
+		return "", ErrNotFound
+	}
+	return value, nil
+}
+
+func (d Dictionary) Add(key, value string) error {
+	_, err := d.Search(key)
+
+	switch err {
+	case ErrNotFound:
+		d[key] = value
+	case nil:
+		return ErrKeyExists
+	default:
+		return err
+	}
+
+	return nil
+}
+
+func (e DictionaryErr) Error() string {
+	return string(e)
 }
--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -5,15 +5,70 @@ import (
 )
 
 func TestSearch(t *testing.T) {
-	dictionary := map[string]string{"test": "this is just a test"}
+	// dictionary := map[string]string{"test": "this is just a test"}
+	dictionary := Dictionary{"test": "this is just a test"}
 
-	got := Search(dictionary, "test")
-	want := "this is just a test"
+	t.Run("known word", func(t *testing.T) {
+		got, _ := dictionary.Search("test")
+		want := "this is just a test"
 
-	assertStrings(t, got, want)
+		assertStrings(t, got, want)
+	})
+
+	t.Run("unknown word", func(t *testing.T) {
+		_, got := dictionary.Search("hithere")
+
+		if got == nil {
+			t.Fatal("expected an error")
+		}
+
+		assertError(t, got, ErrNotFound)
+	})
+}
+
+func TestAdd(t *testing.T) {
+	t.Run("add new word", func(t *testing.T) {
+		dictionary := Dictionary{}
+		word := "added"
+		definition := "added value"
+		err := dictionary.Add(word, definition)
+
+		assertError(t, err, nil)
+		assertDefinition(t, dictionary, word, definition)
+	})
+
+	t.Run("add existing word", func(t *testing.T) {
+		word := "existing"
+		definition := "test value"
+		dictionary := Dictionary{word: definition}
+
+		err := dictionary.Add(word, definition)
+
+		assertError(t, err, ErrKeyExists)
+		assertDefinition(t, dictionary, word, definition)
+	})
+}
+
+func assertDefinition(t *testing.T, dictionary Dictionary, word, definition string) {
+	t.Helper()
+	value, err := dictionary.Search(word)
+
+	if err != nil {
+		t.Fatal("should find word:", word)
+	}
+
+	assertStrings(t, value, definition)
 }
 
 func assertStrings(t testing.TB, got, want string) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+}
+
+func assertError(t testing.TB, got, want error) {
 	t.Helper()
 
 	if got != want {
